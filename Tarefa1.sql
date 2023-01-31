36f8c6cdbe76cc8cd683da365b3111ca9d4b61a4
Romulo Grecco <romulo.grecco@optsol.com.br>
2023-01-31 20:11:57 -0300

    ajustes

--- Tarefa1.sql
+++ Tarefa1.sql
@@ -1,25 +1,25 @@
 CREATE TABLE Departamento (
     Id INT IDENTITY(1,1) PRIMARY KEY,
-    Nome varchar(250) NOT NULL,
+    Nome VARCHAR(100) NOT NULL,
 );
 
 CREATE TABLE Pessoa (
     Id INT IDENTITY(1,1) PRIMARY KEY,
-    Nome varchar(250) NOT NULL,
-    Salario decimal(18,2) NOT NULL,
+    Nome VARCHAR(100) NOT NULL,
+    Salario DECIMAL(18,2) NOT NULL,
   	DepartamentoId INT FOREIGN KEY REFERENCES Departamento(Id)
 );
 
-INSERT INTO Departamento (nome) VALUES ('TI');
-INSERT INTO Departamento (nome) VALUES ('Vendas');
+INSERT INTO Departamento (Nome) VALUES ('TI');
+INSERT INTO Departamento (Nome) VALUES ('Vendas');
 
-INSERT INTO Pessoa (nome, salario, departamentoid) VALUEs ('Joe', 70000, 1);
-INSERT INTO Pessoa (nome, salario, departamentoid) VALUEs ('Henry', 80000, 2);
-INSERT INTO Pessoa (nome, salario, departamentoid) VALUEs ('Sam', 60000, 2);
-INSERT INTO Pessoa (nome, salario, departamentoid) VALUEs ('Max', 90000, 1);
+INSERT INTO Pessoa (Nome, Salario, Departamentoid) VALUES ('Joe', 70000, 1);
+INSERT INTO Pessoa (Nome, Salario, Departamentoid) VALUES ('Henry', 80000, 2);
+INSERT INTO Pessoa (Nome, Salario, Departamentoid) VALUES ('Sam', 60000, 2);
+INSERT INTO Pessoa (Nome, Salario, Departamentoid) VALUES ('Max', 90000, 1);
 
 
-SELECT	d.nome, p.nome, p.salario 
+SELECT	d.Nome, p.Nome, p.Salario 
   from 	Pessoa p 
-  JOIN 	Departamento d ON d.Id = p.departamentoid
- WHERE 	p.salario IN (SELECT MAX(salario) FROM Pessoa GROUP BY departamentoid)
+  JOIN 	Departamento d ON d.Id = p.Departamentoid
+ WHERE 	p.Salario IN (SELECT MAX(Salario) FROM Pessoa GROUP BY Departamentoid)
